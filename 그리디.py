6b099f08ea33385fb9455a1a3c470d0a1e72e288
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-10-10 20:03:36 +0900

    Add files via upload

--- 그리디.py
+++ 그리디.py
@@ -66,21 +66,21 @@ print(result)"""
 
 
 # 1이 될 때까지
-"""n,k = map(int, input().split())
-result = 0
-
-while n >= k:
-    while n % k != 0:
-        n -= 1
-        result += 1
-    n //= k
-    result += 1
-
-while n > 1:
-    n -= 1
-    result += 1
-
-print(result)"""
+# n,k = map(int, input().split())
+# result = 0
+#
+# while n >= k:
+#     while n % k != 0:
+#         n -= 1
+#         result += 1
+#     n //= k
+#     result += 1
+#
+# while n > 1:
+#     n -= 1
+#     result += 1
+#
+# print(result)
 
 
 ######################## 3장 문제 ########################
@@ -89,29 +89,51 @@ print(result)"""
 # 모험가 길드
 # n = int(input())
 #
-# fear = list(map(int, input().split()))
+# data = list(map(int,input().split()))
+# data.sort()
 #
-# fear.sort()
+# group_su = 0
+# mohumga_su = 0
 #
-# result = 0 # 총 그룹의 수
-# count = 0 # 현재 그룹의 인원 수
+# for i in data:   # 현재 그룹에 포함된 모험가의 수가 현재 확인하고 있는 공포도보다 크거나 같다면 그룹 결성
+#     mohumga_su += 1
+#     if mohumga_su >= i:
+#         group_su +=1
+#         mohumga_su = 0
 #
-# for horror in fear:
-#     count += 1  # 현재 그룹의 인원 추가
-#     if (count >= horror):
-#         result += 1
-#         count = 0
-#
-# print(result)
+# print(group_su)
+
 
 # 곱하기 혹은 더하기
-# s = list(map(int,input()))
+# data = input()
 #
-# result = s[0]
+# result = int(data[0]) # 첫 번째 문자를 숫자로 변경하여 대입
 #
-# for i in range(1, len(s)): # 두번째 정수부터 탐색 시작
-#     if(s[i] <= 1 or result <= 1):
-#         result += s[i]
+# for i in range(1, len(data)):  # 1부터 시작
+#     num = int(data[i])
+#     if num <= 1 or result <=1:
+#         result += num
 #     else:
-#         result *= s[i]
+#         result *= num
 # print(result)
+
+# 문자열 뒤집기
+# data = input()
+#
+# count0 = 0  # 전부 0으로 바꾸는 경우
+# count1 = 0
+#
+# if data[0] == '1':
+#     count0 += 1
+# else:
+#     count1 += 1
+#
+# for i in range(len(data)-1):
+#     if data[i] != data[i+1]:
+#         if data[i+1] == '1':
+#             count0 += 1
+#         else:
+#             count1 += 1
+#
+# print(min(count0,count1))
+#
